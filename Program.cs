cebd838cb5e60fd3889511d5e9f687a1a7df1f10
aremo90 <mrhamza2005@gmail.com>
2025-08-10 09:52:27 +0300

    Question 01

--- Program.cs
+++ Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question 1  
+
+            Range<int> Range = new Range<int>(40, 60);
+            Console.WriteLine("enter a number");
+            int input = int.Parse(Console.ReadLine()); 
+            if (Range.IsInRange(input))
+            {
+                Console.WriteLine("the number you entered is in Range");
+            }
+            else
+            {
+                Console.WriteLine("the number you entered is not in Range");
+            }
+
+            #endregion
         }
     }
 }
---
+++ Range.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Range<T> where T : IComparable<T>
+    {
+        public T Min { get; }
+        public T Max { get; }
+
+        public Range(T min, T max)
+        {
+            if (min.CompareTo(max) < 0)
+            {
+                Min = min;
+                Max = max;
+            }
+
+        }
+    }
+}
